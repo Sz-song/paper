c8068afb885f845a79c4ce2e04597d5575daa411
yuanyu <yuanyu@123.com>
2019-01-03 13:34:03 +0800

    2

---
+++ app/src/main/java/com/example/song/paper/login/view/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.song.paper.login.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.song.paper.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
